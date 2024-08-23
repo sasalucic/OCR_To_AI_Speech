a2f8296e53b4914f53af53ac235fd477ea2d6bad
Saša Lučić <134293326+sasalucic@users.noreply.github.com>
2024-08-23 14:00:10 +0200

    Update settings.py

--- ocrwebapp/ocrwebapp/settings.py
+++ ocrwebapp/ocrwebapp/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=!ql-=vq^9)@at0*9q@&8)=aqrfzi_c#4n%5uu0fxn%r_l5#+s"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
